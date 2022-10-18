77c137a9c98beedc3fee9e5b63afd5bfb2b3d2c0
GDDario <gd.dario110@gmail.com>
2022-10-18 01:25:40 -0300

    initial commit

---
+++ src/main/java/com/gabriel/breadstore/BreadstoreApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.breadstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/breadstore/BreadstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.breadstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
